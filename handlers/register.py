a7c1b9a0d133879e4626491caebbe3a5c37f0d79
EAzizbek <a.ergashov1308@gmail.com>
2026-02-10 19:27:50 +0500

    First commit

---
+++ handlers/register.py
@@ -0,0 +1,39 @@
+from aiogram.fsm.context import FSMContext
+from aiogram import F,Router
+from aiogram.types import Message
+from states.register import RegisterState
+
+router=Router()
+
+@router.message(F.text=="Register")
+async def register(msg:Message,state:FSMContext):
+    await msg.answer("Registiratsiyadan o'tish uchun iltimos ismingizni kiriting!")
+    await state.set_state(RegisterState.name)
+
+@router.message(RegisterState.name)
+async def register(msg:Message,state:FSMContext):
+    await state.update_data(name=msg.text)
+    await msg.answer("Familyangizni kiriting")
+    await state.set_state(RegisterState.surename)
+
+@router.message(RegisterState.surename)
+async def register(msg:Message,state:FSMContext):
+    await state.update_data(surename=msg.text)
+    await msg.answer("Yoshingizni kiriting")
+    await state.set_state(RegisterState.age)
+
+@router.message(RegisterState.age)
+async def register(msg:Message,state:FSMContext):
+    await state.update_data(age=msg.text)
+    await msg.answer("Telfon raqamingizni kiriting")
+    await state.set_state(RegisterState.phone)
+
+@router.message(RegisterState.phone)
+async def register(msg:Message,state:FSMContext):
+    await state.update_data(phone=msg.text)
+
+    data = await state.get_data()
+
+    await msg.answer(f'Ismingiz: {data['name']}\nFamilyangiz: {data["surename"]}\nYoshingiz: {data['age']}\nTelefon raqamingiz: {data["phone"]}')
+    await state.clear()
+
